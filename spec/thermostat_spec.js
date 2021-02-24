a494aaea340f53c3320fd703286c735bcd97cb56
mitchell9-1 <mitchell.perry001@gmail.com>
2021-02-24 13:52:06 +0000

    Added Tests

--- spec/thermostat_spec.js
+++ spec/thermostat_spec.js
@@ -9,20 +9,33 @@ describe('Thermostat', function()
     thermostat = new Thermostat();
   });
 
-  it('starts at 20 degrees', function() 
+  it('Starts at 20 degrees', function() 
   {
     expect(thermostat.temperature).toEqual(20);
   });
 
   it('Increases the temperature by 1', function() 
   {   
-    thermostat.up()
+    thermostat.up();
     expect(thermostat.temperature).toEqual(21);
   });
-  
+
   it('Decreases the temperature by 1', function() 
   {   
     thermostat.down()
     expect(thermostat.temperature).toEqual(19);
   });
+
+
+  it('Has Power Saving Mode enabled by default', function() {
+    thermostat.is_power_saving_mode();
+    expect(thermostat.is_power_saving_mode()).toEqual(true);
+  });
+
+  it('Has Power Saving Mode enabled by default', function() {
+    expect(thermostat.power_saving_mode_off()).toEqual(false);
+  });
+
+
+
 });
