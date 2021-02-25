9d2f22396cee2c5c267504655d7d3aa31c78e994
mitchell9-1 <mitchell.perry001@gmail.com>
2021-02-25 19:31:41 +0000

    Setup JQuery and configured the buttons from thermostat.js

---
+++ src/interface.js
@@ -0,0 +1,38 @@
+
+$(document).ready(function() {
+
+  var thermostat = new Thermostat();
+  updateTemperature();
+
+  $('#temperature-up').click(function() {
+    thermostat.up();
+    updateTemperature();
+  });
+
+  $('#temperature-down').click(function() {
+    thermostat.down();
+    updateTemperature();
+  });
+
+  $('#temperature-reset').click(function() {
+    thermostat.resetTemperature();
+    updateTemperature();
+  });
+
+  $('#powersaving-on').click(function() {
+    thermostat.power_saving_mode_on();
+    $('#power-saving').text('On')
+    updateTemperature();
+  })
+
+  $('#powersaving-off').click(function() {
+    thermostat.power_saving_mode_off();
+    $('#power-saving').text('Off')
+    updateTemperature();
+  })
+
+  function updateTemperature() {
+  $('#temperature').text(thermostat.temperature);
+  $('#temperature').attr('class', thermostat.energyUsage());
+  }
+});
